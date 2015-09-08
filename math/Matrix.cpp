2a349d026c8f0b8c94329c5f412f13b59f32ca4c
Alexander Samarin <samarin_ad@mail.ru>
2015-09-08 17:39:25 +0300

    Update Matrix.cpp

--- math/Matrix.cpp
+++ math/Matrix.cpp
@@ -1,9 +1,34 @@
 #include "Matrix.h"
 
-Matrix::Matrix(unsigned height, unsigned width)
+Matrix::Matrix(const size_t height, const size_t width, const double initial_value) :
+    n(height),
+    m(width),
+    data(n * m, initial_value)
+{}
+
+Matrix::Matrix(const Matrix & other) :
+    n(other.n),
+    m(other.m),
+    data(other.data)
+{}
+
+Matrix & Matrix::operator=(const Matrix & other)
 {
-    n = height;
-    m = width;
-    data.reserve(n * m);
+    if (this != & other)
+    {
+        n = other.n;
+        m = other.m;
+        data = other.data;
+    }
+    return *this;
 }
 
+double Matrix::operator()(const size_t i, const size_t j) const
+{
+    return data[i * n + j];
+}
+
+double & Matrix::operator()(const size_t i, const size_t j)
+{
+    return data[i * n + j];
+}
