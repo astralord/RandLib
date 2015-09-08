d7b1cbd99f46597f7e2b01f03f3c13e59a7ae11f
Alexander Samarin <samarin_ad@mail.ru>
2015-09-08 14:57:16 +0300

    Update ChiSquaredRand.h

--- variables/continuous/ChiSquaredRand.h
+++ variables/continuous/ChiSquaredRand.h
@@ -9,22 +9,12 @@
  */
 class RANDLIBSHARED_EXPORT ChiSquaredRand : public GammaRand
 {
-    int k;
-    double pdfCoef, cdfCoef;
-
 public:
     explicit ChiSquaredRand(int degree = 1);
     virtual std::string name() override;
 
     void setDegree(int degree);
-    inline int getDegree() const { return k; }
-
-    double E() const override { return k; }
-    double Var() const override { return k + k; }
-
-    inline double Mode() const { return std::max(k - 2, 0); }
-    inline double Skewness() const { return std::sqrt(8.0 / k); }
-    inline double ExcessKurtosis() const { return 12.0 / k; }
+    inline int getDegree() const { return k + k; }
 };
 
 #endif // CHISQUAREDRAND_H
