b53e8f857bfe9526dccc3941c3fa42d3b5f53692
Alexander Samarin <samarin_ad@mail.ru>
2015-09-08 14:52:00 +0300

    Update BetaPrimeRand.h

--- variables/continuous/BetaPrimeRand.h
+++ variables/continuous/BetaPrimeRand.h
@@ -29,6 +29,20 @@ public:
         double denominator = (betaAdj - 1) * betaAdj * betaAdj;
         return numerator / denominator;
     }
+    
+    inline double Mode() { return (X.getShape() < 1) ? 0 : (X.getShape() - 1) / (Y.getShape() + 1);
+    double Skewness() {
+        double alpha = X.getShape();
+        double beta = Y.getShape();
+        if (beta <= 3)
+            return INFINITY;
+        double aux = alpha + beta - 1;
+        double skewness = (beta - 2) / (alpha * aux);
+        skewness = std::sqrt(skewness);
+        aux += alpha;
+        aux += aux;
+        return aux * skewness / (beta - 3);
+    }
 };
 
 #endif // BETAPRIMERAND_H
