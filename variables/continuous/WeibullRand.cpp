343b3b9a9ab831e07633b92ab3eca2769b59cf69
Alexander Samarin <samarin_ad@mail.ru>
2015-09-10 19:59:32 +0300

    Update WeibullRand.cpp

--- variables/continuous/WeibullRand.cpp
+++ variables/continuous/WeibullRand.cpp
@@ -15,6 +15,7 @@ void WeibullRand::setParameters(double scale, double shape)
     l = std::max(scale, MIN_POSITIVE);
     k = std::max(shape, MIN_POSITIVE);
     lInv = 1.0 / l;
+    kInv = 1.0 / k;
 }
 
 double WeibullRand::f(double x) const
@@ -35,18 +36,60 @@ double WeibullRand::F(double x) const
 
 double WeibullRand::variate() const
 {
-    return l * std::pow(ExponentialRand::standardVariate(), 1.0 / k);
+    return l * std::pow(ExponentialRand::standardVariate(), kInv);
 }
 
 double WeibullRand::E() const
 {
-    return l * std::tgamma(1 + 1.0 / k);
+    return l * std::tgamma(1 + kInv);
 }
 
 double WeibullRand::Var() const
 {
-    double res = std::tgamma(1 + 1.0 / k);
+    double res = std::tgamma(1 + kInv);
     res *= res;
-    res += std::tgamma(1 + 2.0 / k);
+    res += std::tgamma(1 + kInv + kInv);
     return l * l * res;
+}
+
+double WeibullRand::Median() const
+{
+    return l * std::pow(M_LN2, kInv);
+}
+
+double WeibullRand::Mode() const
+{
+    if (k < 1)
+        return INFINITY;
+    return l * std::pow(1 - kInv, kInv);
+}
+
+double WeibullRand::Skewness() const
+{
+    double mu = E();
+    double var = Var();
+    double sigma = std::sqrt(var);
+    double numerator = std::tgamma(1 + 3.0 * kInv);
+    numerator *= lambda * lambda * lambda;
+    numerator -= 3 * mu * var;
+    numerator -= mu * mu * mu;
+    double denominator = var * sigma;
+    return numerator / denominator;
+}
+
+double WeibullRand::ExcessKurtosis() const
+{
+    double mu = E();
+    double var = Var();
+    double sigma = std::sqrt(var);
+    double skewness = Skewness();
+    double numerator = l * l;
+    numerator *= numerator;
+    numerator *= std::tgamma(1 + 4.0 * kInv);
+    numerator -= 4 * skewness * var * sigma * mu;
+    double mu2 = mu * mu;
+    numerator -= 6 * mu2 * var;
+    numerator -= mu2 * mu2;
+    double kurtosis = numerator / (var * var);
+    return kurtosis - 3;
 }
