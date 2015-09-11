603d810d7b456c626b45fa71e4460d8672ac8271
Alexander Samarin <samarin_ad@mail.ru>
2015-09-11 17:24:37 +0300

    Update NegativeBinomialRand.h

--- variables/discrete/NegativeBinomialRand.h
+++ variables/discrete/NegativeBinomialRand.h
@@ -36,6 +36,11 @@ private:
 public:
     double E() const override;
     double Var() const override;
+    
+    double Median() const override;
+    double Mode() const override;
+    double Skewness() const override;
+    double ExcessKurtosis() const override;
 };
 
 typedef NegativeBinomialRand<int> PascaleRand;
