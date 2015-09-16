e7b9f0bc2b12bee69b836ff3e20f7eccdeee8e5e
Alexander Samarin <samarin_ad@mail.ru>
2015-09-16 16:46:41 +0300

    Create WignerSemicircleRand.h

---
+++ variables/continuous/WignerSemicircleRand.h
@@ -0,0 +1,34 @@
+#ifndef WIGNERSEMICIRCLERAND_H
+#define WIGNERSEMICIRCLERAND_H
+
+#include "BetaRand.h"
+
+class RANDLIBSHARED_EXPORT WignerSemicircleRand : public ContinuousRand
+{
+    double R, RSq
+    BetaRand X; /// for generator
+    
+public:
+    explicit WignerSemicircleRand(double radius);
+    std::string name() override;
+
+    void setRadius(double radius);
+    inline double getRadius() const { return R; }
+    
+public:
+    double f(double x) const override;
+    double F(double x) const override;
+    double variate() const override;
+
+    double Mean() const override;
+    double Variance() const override;
+
+    double Median() const override;
+    double Mode() const override;
+    double Skewness() const override;
+    double ExcessKurtosis() const override;
+    
+    double Entropy() const;
+};
+
+#endif // WIGNERSEMICIRCLERAND_H
