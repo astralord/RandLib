4f7a14b00f64f8a1e5ad4febd4c117625e69a58f
Aleksandr <samarin_ad@mail.ru>
2016-04-02 14:24:19 +0200

    Added Log-Cauchy distribution

---
+++ distributions/univariate/continuous/LogCauchyRand.h
@@ -0,0 +1,39 @@
+#ifndef LOGCAUCHYRAND_H
+#define LOGCAUCHYRAND_H
+
+#include "ContinuousDistribution.h"
+#include "CauchyRand.h"
+
+/**
+ * @brief The LogCauchyRand class
+ */
+class RANDLIBSHARED_EXPORT LogCauchyRand : public ContinuousDistribution
+{
+    double expMu, expVar;
+    CauchyRand X;
+
+public:
+    LogCauchyRand(double location = 0, double scale = 1);
+    std::string name() override;
+
+    void setLocation(double location);
+    void setScale(double scale);
+    inline double getLocation() const { return X.getLocation(); }
+    inline double getScale() const { return X.getScale(); }
+
+    double f(double x) const override;
+    double F(double x) const override;
+    double variate() const override;
+
+    double Mean() const override;
+    double Variance() const override;
+
+    double Quantile(double p) const override;
+
+    double Median() const override;
+    double Mode() const override;
+    double Skewness() const override;
+    double ExcessKurtosis() const override;
+};
+
+#endif // LOGCAUCHYRAND_H
